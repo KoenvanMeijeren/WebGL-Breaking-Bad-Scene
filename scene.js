839b990c5d39aff8378fa4d1727f2459bcf8e50c
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-02-10 12:12:49 +0100

    Implement examples of WebGL

--- scene.js
+++ scene.js
@@ -6,16 +6,24 @@ const camera = new THREE.PerspectiveCamera(
     75,     // fov - Camera frustum vertical field of view
     window.innerWidth / window.innerHeight, // aspect - Camera frustum aspect ratio
     0.1,   // near - Camera frustum near plane
-    1000); // far - Camera frustum far plane
+    1000   // far - Camera frustum far plane
+);
 
 // Create renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementsByClassName('scene-container')[0].appendChild(renderer.domElement);
+
+const materials = [];
+materials.push(new THREE.MeshNormalMaterial());
+materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000 }));
+materials.push(new THREE.MeshPhongMaterial({ color: 0xad0000 , shininess: 100 }));
+materials.push(new THREE.MeshLambertMaterial({color: 0xad0000, shininess: 100 }));
+materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000, transparent: true, opacity:.2 }));
+materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000, wireframe: true }));
 
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(geometry, materials[0]);
 scene.add(cube);
 
 // Move camera from center
@@ -27,14 +35,28 @@ renderer.render(scene, camera);
 
 const clock = new THREE.Clock();
 
-const render = function () {
-    requestAnimationFrame(render);
+const rotateCube = function () {
+    requestAnimationFrame(rotateCube);
     const delta = clock.getDelta();
 
     cube.rotation.x += 3.2 * delta;
     cube.rotation.y += 3.2 * delta;
 
     renderer.render(scene, camera);
-};
+}
 
-render();
+const changeCubeMaterial = function (index) {
+    cube.material = materials[index];
+    renderer.render(scene, camera);
+}
+
+const addLight = function () {
+    const light = new THREE.DirectionalLight(0xdddd00, 1);
+    light.position.set(0, 0, 1 );
+    scene.add(light);
+    renderer.render(scene, camera);
+}
+
+const resetScene = function () {
+    window.location.reload();
+}
