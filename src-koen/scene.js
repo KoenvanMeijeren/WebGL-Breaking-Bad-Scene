052c39f638364f7deb33cc4eccd0d1e8d28457a4
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-02-20 08:57:36 +0100

    Implement three js playground

--- src-koen/scene.js
+++ src-koen/scene.js
@@ -1,64 +1,148 @@
-// Create scene
-const scene = new THREE.Scene();
+// Define camera
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
+camera.position.set(-900, -200, -900);
 
-// Create camera
-const camera = new THREE.PerspectiveCamera(
-    75,     // fov - Camera frustum vertical field of view
-    window.innerWidth / window.innerHeight, // aspect - Camera frustum aspect ratio
-    0.1,   // near - Camera frustum near plane
-    1000   // far - Camera frustum far plane
-);
-
-// Create renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-const sceneContainer = document.getElementsByClassName('scene-container')[0];
-sceneContainer.innerHTML = "";
-sceneContainer.appendChild( renderer.domElement );
-
-const materials = [];
-materials.push(new THREE.MeshNormalMaterial());
-materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000 }));
-materials.push(new THREE.MeshPhongMaterial({ color: 0xad0000 , shininess: 100 }));
-materials.push(new THREE.MeshLambertMaterial({color: 0xad0000, shininess: 100 }));
-materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000, transparent: true, opacity:.2 }));
-materials.push(new THREE.MeshBasicMaterial({ color: 0xad0000, wireframe: true }));
-
-const geometry = new THREE.BoxGeometry(1,1,1);
-const cube = new THREE.Mesh(geometry, materials[0]);
-scene.add(cube);
-
-// Move camera from center
-camera.position.x = 2; // move right from center of scene
-camera.position.y = 1; // move up from center of scene
-camera.position.z = 5; // move camera away from center of scene
-
-renderer.render(scene, camera);
-
+// Define clock
 const clock = new THREE.Clock();
 
-const rotateCube = function () {
-    requestAnimationFrame(rotateCube);
+// Define scene
+const scene = new THREE.Scene();
+
+// Define renderer.
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Define model loaders
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new THREE.GLTFLoader();
+
+const mixers = [];
+
+init();
+
+function init() {
+    addOrbitControls();
+    addSkybox();
+    addLights();
+    addFlyingFlamingo();
+
+    animate();
+}
+
+function addLights() {
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 50, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.color.setHSL(0.1, 1, 0.95);
+    dirLight.position.set(-1, 1.75, 1);
+    dirLight.position.multiplyScalar(50);
+    scene.add(dirLight);
+}
+
+function addOrbitControls() {
+    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 500;
+    controls.maxDistance = 1500;
+}
+
+function addSkybox() {
+    let materials = [];
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/posx.jpg'),
+        side: THREE.BackSide
+    }));
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/negx.jpg'),
+        side: THREE.BackSide
+    }));
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/posy.jpg'),
+        side: THREE.BackSide
+    }));
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/negy.jpg'),
+        side: THREE.BackSide
+    }));
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/posz.jpg'),
+        side: THREE.BackSide
+    }));
+    materials.push(new THREE.MeshBasicMaterial({
+        map: textureLoader.load('./skybox/negz.jpg'),
+        side: THREE.BackSide
+    }));
+
+    let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+    let skybox = new THREE.Mesh(skyboxGeo, materials);
+    scene.add(skybox);
+}
+
+var flamingo = null;
+function addFlyingFlamingo() {
+    gltfLoader.load('./models/Flamingo.glb', function (gltf) {
+        flamingo = gltf.scene.children[0];
+
+        const scale = 3;
+        flamingo.scale.set(scale, scale, scale);
+        flamingo.position.x = -500;
+        flamingo.position.y = 100;
+        flamingo.position.z = 1500;
+        flamingo.rotation.y = 1.5;
+        scene.add(flamingo);
+
+        const mixer = new THREE.AnimationMixer(flamingo);
+        mixer.clipAction(gltf.animations[0]).setDuration(1).play();
+        mixers.push(mixer);
+    });
+}
+
+const startZPosition = -5000, endZPosition = 5000;
+let hasReachedEnd = false, hasReachedStart = false;
+function animateFlyingFlamingo() {
+    if (flamingo == null) {
+        return;
+    }
+
+    if (!hasReachedEnd && flamingo.position.x < endZPosition) {
+        flamingo.position.x += 50;
+    }
+
+    if (flamingo.position.x === endZPosition) {
+        hasReachedEnd = true;
+        flamingo.rotation.y = -1.5;
+    }
+
+    if (hasReachedEnd && flamingo.position.x > startZPosition) {
+        flamingo.position.x -= 50;
+    }
+
+    if (flamingo.position.x === startZPosition) {
+        hasReachedStart = true;
+        flamingo.rotation.y = 1.5;
+    }
+
+    if (hasReachedStart && hasReachedEnd) {
+        hasReachedStart = false;
+        hasReachedEnd = false;
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    animateFlyingFlamingo();
+    render();
+}
+
+function render() {
     const delta = clock.getDelta();
-
-    cube.rotation.x += 3.2 * delta;
-    cube.rotation.y += 3.2 * delta;
+    for (let index = 0; index < mixers.length; index++) {
+        mixers[index].update(delta);
+    }
 
     renderer.render(scene, camera);
-}
-
-const changeCubeMaterial = function (index) {
-    cube.material = materials[index];
-    renderer.render(scene, camera);
-}
-
-const addLight = function () {
-    const light = new THREE.DirectionalLight(0xdddd00, 1);
-    light.position.set(0, 0, 1 );
-    scene.add(light);
-    renderer.render(scene, camera);
-}
-
-const resetScene = function () {
-    window.location.reload();
 }
