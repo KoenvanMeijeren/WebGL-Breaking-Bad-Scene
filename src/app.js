db4e95d5dd295ac3398167d1a33ca78031d95bcb
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-02-21 09:28:53 +0100

    Add background music

---
+++ src/app.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    init();
+
+    // Work-around for cannot play music without user interaction.
+    document.getElementById("music-start-btn").click();
+});
+
+function init() {
+    const startBtn = document.getElementById("music-start-btn");
+    const music = document.getElementById("background-music");
+
+    startBtn.addEventListener("click", () => {
+        music.volume = 0.2;
+        music.repeat = true;
+        music.play();
+    });
+}
